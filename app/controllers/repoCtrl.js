50f5ef7f6f60aaff85a3a668cb9b9e42bad958f3
rze89 <rze89@yahoo.com>
2016-07-22 13:00:10 -0500

    some touch ups

--- app/controllers/repoCtrl.js
+++ app/controllers/repoCtrl.js
@@ -6,6 +6,8 @@ ghApp.controller('repoCtrl', function repoCtrlController($routeParams, $scope, g
     $scope.repo;
     $scope.languages = [];
 
+
+
     function makeList(obj) {
         var language;
         var total = 0;
@@ -25,11 +27,19 @@ ghApp.controller('repoCtrl', function repoCtrlController($routeParams, $scope, g
         }
         return list;
     }
+    function formatDate(date){
+      var tempDate = moment(date.toString()).format('MMMM-DD-YYYY');
+      var tempTime = moment(date.toString()).format('h:mm a');
+      var result = tempDate + " at " + tempTime;
 
+      return result;
+    }
 
     ghServices.getRepo($scope.username, $scope.reponame).success(function(data) {
         $scope.repo = data;
-
+        $scope.updated_at = formatDate($scope.repo.updated_at);
+        $scope.created_at = formatDate($scope.repo.created_at);
+        $scope.pushed_at = formatDate($scope.repo.pushed_at);
         ghServices.getLanguages($scope.repo.languages_url).success(function(data) {
             var temp = data;
             $scope.languages = makeList(temp);
