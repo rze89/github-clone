f48d80217ebfec75f6bdeeff30ca1590e595b0db
rze89 <rze89@yahoo.com>
2016-07-24 21:35:56 -0500

    Added compare method and edited reset method

--- app/controllers/compareCtrl.js
+++ app/controllers/compareCtrl.js
@@ -4,29 +4,30 @@ ghApp.controller('compareCtrl', function compareCtrlController($scope, ghService
 
   $scope.user1name = $routeParams.username;
   //console.log($location.search($scope.username));
-  $scope.user2name;
   $scope.user;
+  $scope.user2name;
   $scope.didCompare;
-
-  function repoCount(repos){
-    var count = 0;
-    angular.forEach(repos, function(item){
-      count++;
-    });
-    return count;
-  }
 
   ghServices.getGithubData($scope.user1name).success(function(data){
       $scope.user = data;
       ghServices.getRepos($scope.user.repos_url).success(function(data){
-          $scope.repos = data;
-          $scope.user1RepoTotal = repoCount($scope.repos);
+          $scope.user1Repos = data;
+          $scope.user1RepoTotal = repoCount($scope.user1Repos);
+          $scope.user1TotalStars = getTotalStars($scope.user1Repos);
+      });
+      ghServices.getFollowing($scope.user).success(function(data){
+          var temp = data;
+          $scope.user1totalFollowing = Object.keys(temp).length;
       });
 
   });
   $scope.clearUser = function(user){
     $scope.user2 = null;
     $scope.searchParam = "";
+    $scope.didCompare = false;
+  }
+  $scope.compare = function(){
+    $scope.didCompare = true;
   }
   $scope.searchUser = function(){
     ghServices.getGithubData($scope.searchParam).success(function(data){
@@ -34,10 +35,33 @@ ghApp.controller('compareCtrl', function compareCtrlController($scope, ghService
       ghServices.getRepos($scope.user2.repos_url).success(function(data){
           $scope.user2Repos = data;
           $scope.user2RepoTotal = repoCount($scope.user2Repos);
+          $scope.user2TotalStars = getTotalStars($scope.user2Repos);
+      });
+      ghServices.getFollowing($scope.user2).success(function(data){
+          var temp = data;
+          $scope.user2totalFollowing = Object.keys(temp).length;
       });
     }).error(function(data){
         $scope.error = "Sorry, we couldn't find a user by that name."
     })
-  }
+  };
+
+  function getTotalStars(repos){
+    var count = 0;
+    angular.forEach(repos, function(repo){
+      if(repo.stargazers_count > 0){
+        count += repo.stargazers_count;
+      }
+    });
+    return count;
+  };
+
+  function repoCount(repos){
+    var count = 0;
+    angular.forEach(repos, function(item){
+      count++;
+    });
+    return count;
+  };
 
 });
